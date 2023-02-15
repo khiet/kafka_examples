0543635ea161fa94e6990ea00916eecd6dea9d6f
Khiet Le <khiet@outlook.com>
2023-02-15 19:27:21 +0000

    Add a JS example consumer with kafkajs

---
+++ kafka.js
@@ -0,0 +1,32 @@
+// KafkaJS: https://github.com/tulios/kafkajs
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+  clientId: 'kafkaJS',
+  brokers: process.env.KAFKA_BOOTSTRAP_SERVERS.split(','),
+  ssl: true,
+  sasl: {
+    mechanism: 'scram-sha-512',
+    username: process.env.KAFKA_SASL_USERNAME,
+    password: process.env.KAFKA_SASL_PASSWORD
+  },
+});
+
+const consumer = kafka.consumer({ groupId: 'kafka-js' });
+
+const run = async () => {
+  await consumer.connect()
+  await consumer.subscribe({ topic: process.env.TOPIC, fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      })
+    },
+  })
+};
+
+run().catch(console.error);
