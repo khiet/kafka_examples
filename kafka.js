15de1fe92ccad9fb0488619f0730d5b652ec3212
Khiet Le <khiet@outlook.com>
2023-02-19 11:53:47 +0000

    Update the JS example to consume and produce

--- kafka.js
+++ kafka.js
@@ -10,23 +10,42 @@ const kafka = new Kafka({
     username: process.env.KAFKA_SASL_USERNAME,
     password: process.env.KAFKA_SASL_PASSWORD
   },
-});
+})
 
-const consumer = kafka.consumer({ groupId: 'kafka-js' });
+const consumer = kafka.consumer({ groupId: `kafka-js-${Date.now()}` });
+const producer = kafka.producer();
+
+const pipePetsSpending = async (message) => {
+  const jsonMessage = JSON.parse(message.value.toString())
+
+  if (jsonMessage['name'] === 'get_account_transactions') {
+    jsonMessage['input_blob'].forEach(async (item) => {
+      if (item['transaction_classification'].includes('Pets') && item.amount < 0) {
+
+        await producer.send({
+          topic: 'bank_connections_demo',
+          messages: [
+            {
+              value: JSON.stringify({
+                transaction_classification: 'Pets',
+                amount: Math.abs(item.amount)
+              })
+            },
+          ],
+        })
+      }
+    })
+  }
+}
 
 const run = async () => {
   await consumer.connect()
+  await producer.connect()
+
   await consumer.subscribe({ topic: process.env.KAFKA_TOPIC, fromBeginning: true })
-
   await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        partition,
-        offset: message.offset,
-        value: message.value.toString(),
-      })
-    },
+    eachMessage: async ({ _topic, _partition, message }) => await pipePetsSpending(message),
   })
-};
+}
 
-run().catch(console.error);
+run().catch(console.error)
